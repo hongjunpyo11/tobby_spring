b445d60e3b5418391f108197d5c3e549ff62c76b
hongjunpyo11 <jpyo98@naver.com>
2023-02-21 18:56:05 +0900

    @Component 스캔

--- src/main/java/tobyspring/helloboot/SimpleHelloService.java
+++ src/main/java/tobyspring/helloboot/SimpleHelloService.java
@@ -1,7 +1,11 @@
 package tobyspring.helloboot;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import java.util.Objects;
 
+@Service
 public class SimpleHelloService implements HelloService {
     @Override
     public String sayHello(String name) {
