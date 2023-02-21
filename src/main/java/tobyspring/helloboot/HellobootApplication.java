a9c569d7454cb028394d3898073735c9a51497de
hongjunpyo11 <jpyo98@naver.com>
2023-02-21 22:55:19 +0900

    SpringBootApplication

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,5 +1,6 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -23,22 +24,13 @@ public class HellobootApplication {
     }
 
     public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                            .addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-        applicationContext.register(HellobootApplication.class);
-        applicationContext.refresh(); // 스프링 컨테이너의 초기화 작업
+        SpringApplication.run(HellobootApplication.class, args);
     }
 }
+
+/**
+ * 마지막은 우리가 만든 MySpringApplication 대신 스프링 부트에서 사용하는 SpringApplication 을 사용하였다.
+ * 그렇게 해도 똑같이 작동하는 것을 볼 수 있다.
+ *
+ * 위에 우리가 만들어둔 configuration 이나 BeanFactory method는 아직 필요하다. 왜 이런 부분이 아직 필요한지 알아보자!
+ */
